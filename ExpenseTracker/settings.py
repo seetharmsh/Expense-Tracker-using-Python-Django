d7bae16354afe7465750823bb4824ff99bfe9b58
Seetha R <seetha.rmsh@gmail.com>
2024-07-04 18:42:09 +0530

    Update settings.py

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -127,4 +127,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'seetha.rmsh@gmail.com'
-EMAIL_HOST_PASSWORD = 'begyzejtvmvhkswm'
+EMAIL_HOST_PASSWORD = ''
